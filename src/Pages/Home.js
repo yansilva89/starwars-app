5a3ab2329cec3d42c8e8ee01a77b7c1c0658bcc7
Yan Silva <yansilva@MacBook-Pro-de-Yan.local>
2020-11-06 15:34:23 -0300

    Extra commit title

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,10 +4,16 @@ import './Home.scss'
 // Img
 import swbanner from '../assets/img/swbanner.png'
 import swbanner2 from '../assets/img/swbanner2.jpg'
+//
+import HeadTitle from '../Components/HeadTitle/HeadTitle'
 
 const Home = () => {
   return (
     <div className="home animeLeft">
+      <HeadTitle 
+        title={`StarWars App`}
+        description={`StarWars App | Bem vindo ao StarWars App`}
+      />
       <h1><span>SWAPI</span> | Explore o universo Star Wars</h1>
       <img src={swbanner} alt="Star Wars Banner"/>
       <p>Escolha as opções disponíveis no menu acima e visualize informações de todo o universo Star Wars.</p>
